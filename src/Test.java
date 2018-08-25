2e8a8f2809c258f436b03240baf453c0121bc629
zhengmeng123 <17610750279@163.com>
2018-08-25 22:38:29 +0800

    second5

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+        System.out.println(simpleDateFormat.format(new Date()));
+    }
+
+
+
+
+
+
+
+
+
+}
