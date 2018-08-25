98c48c7391c33c80c84424e3dfd29b6646e72761
zhengmeng123 <17610750279@163.com>
2018-08-25 21:43:48 +0800

    second

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,12 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld1.");
+        System.out.println("HelloWorld2.");
+        System.out.println("HelloWorld3.");
+        System.out.println("HelloWorld4.");
+        System.out.println("HelloWorld5.");
+
+    }
+
+
 }
